592094c46676429ee439b0daa1408ebdf2992d59
Kevin Ahr <meowmeowahr@gmail.com>
2024-02-07 19:13:38 -0600

    maybe working game piece yaw without json

--- src/main/java/frc/robot/subsystems/vision/CustomGamePieceVision.java
+++ src/main/java/frc/robot/subsystems/vision/CustomGamePieceVision.java
@@ -1,15 +1,9 @@
 package frc.robot.subsystems.vision;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.NullNode;
-
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.DoubleTopic;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.StringSubscriber;
-import edu.wpi.first.networktables.StringTopic;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,48 +11,24 @@ import frc.robot.subsystems.Swerve;
 
 public class CustomGamePieceVision extends SubsystemBase{
     private NetworkTableInstance ntInst;
-    private StringTopic pipelineTopic;
-    private StringSubscriber pipelineSubscriber;
+    private DoubleTopic yawTopic;
+    private DoubleSubscriber yawSubscriber;
 
-    private JsonNode objectPipelineResult;
-    private JsonNode bestGamePiece;
+    double gamePieceYaw = 999.0;
 
-    double gamePieceYaw, gamePieceArea, gamePiecePerimeter = 999.0;
-
-    public CustomGamePieceVision(String topicString){
+    public CustomGamePieceVision(String yawString){
         ntInst = NetworkTableInstance.getDefault();
 
         // get a topic from a NetworkTableInstance
         // the topic name in this case is the full name
-        this.pipelineTopic = ntInst.getStringTopic("/Vision/" + topicString);
+        this.yawTopic = ntInst.getDoubleTopic("/Vision/" + yawString);
 
-        this.pipelineSubscriber = this.pipelineTopic.subscribe("{}");
+        this.yawSubscriber = this.yawTopic.subscribe(999.0);
     }
 
     @Override
     public void periodic(){
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            objectPipelineResult = mapper.readTree(pipelineSubscriber.get().toString());
-        } catch (JsonProcessingException e) {
-            gamePieceYaw = 999.0;
-            gamePieceArea = 999.0;
-            gamePiecePerimeter = 999.0;
-            return;
-        }
-
-        bestGamePiece = objectPipelineResult.get("best"); // get best object
-
-        if (bestGamePiece == NullNode.instance || bestGamePiece == null) {
-            // there are no notes
-            gamePieceYaw = 999.0;
-            gamePieceArea = 999.0;
-            gamePiecePerimeter = 999.0;
-        } else {
-            gamePieceYaw = bestGamePiece.get("yaw").asDouble();
-            gamePieceArea = bestGamePiece.get("area").asDouble();
-            gamePiecePerimeter = bestGamePiece.get("perimeter").asDouble();
-        }
+        gamePieceYaw = yawSubscriber.get();
 
         // Update SmartDashboard for game piece
         SmartDashboard.putNumber("Game Piece Yaw", gamePieceYaw);
@@ -73,19 +43,10 @@ public class CustomGamePieceVision extends SubsystemBase{
     }
 
     /**
-     * Gets the Area (px) of the game piece.
-     * @return The Area in px.
-     */
-    public double getGamePieceArea(){
-        return gamePieceArea;
-    }
-
-    /**
      * Align center of camera with game piece
      * @param Swerve drive subsystem
      */
     public void alignNoteYaw(Swerve swerve) {
-        System.out.println("help me");
         swerve.drive(new Translation2d(0, 0), gamePieceYaw/20, false, true);
     }
 

8e05f6054f021b65e23b77ebfa6b23fa3de4da8b
Daniel Margavio <danmargavio@gmail.com>
2024-02-11 10:51:11 -0600

    added intake motor temp monitoring

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -9,7 +9,7 @@ import frc.robot.Constants.SATConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DigitalInput;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.IntakeSubcommands.*;
 import frc.robot.commands.IndexSubcommands.*;
 
@@ -181,6 +181,10 @@ public class Intake extends SubsystemBase {
   
     intakeMotorPos = intakeMotor.getPosition().getValue();
     indexMotorPos = indexMotor.getPosition().getValue();
+
+    SmartDashboard.putNumber("Intake Motor Temperature", intakeMotor.getDeviceTemp().getValueAsDouble());
+    SmartDashboard.putNumber("Index Motor Temperature", indexMotor.getDeviceTemp().getValueAsDouble());
+
   }
   
   // USE FOR TESTING ALSO

dd2ec463b91fd78a0df6c63ae6d9a86f357ad509
Gaurav Salandri <salangau000@gmail.com>
2024-03-05 09:05:40 -0600

    potential apriltag fix

--- src/main/java/frc/robot/subsystems/vision/ApriltagVision.java
+++ src/main/java/frc/robot/subsystems/vision/ApriltagVision.java
@@ -1,80 +1,89 @@
-// package frc.robot.subsystems.vision;
+package frc.robot.subsystems.vision;
 
-// import java.util.Optional;
+import java.util.Optional;
 
-// import org.photonvision.EstimatedRobotPose;
-// import org.photonvision.PhotonCamera;
-// import org.photonvision.PhotonPoseEstimator;
-// import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-// import org.photonvision.estimation.TargetModel;
-// import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.estimation.TargetModel;
+import org.photonvision.targeting.PhotonPipelineResult;
 
-// import edu.wpi.first.apriltag.AprilTagFieldLayout;
-// import edu.wpi.first.apriltag.AprilTagFields;
-// import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.wpilibj.Timer;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants;
-// import frc.robot.RobotContainer;
-// import frc.robot.subsystems.Swerve;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Swerve;
 
-// public class ApriltagVision extends SubsystemBase {
+public class ApriltagVision extends SubsystemBase {
     
-//     private PhotonCamera mFrontRightCam, mBackLeftCam;
-//     private PhotonPoseEstimator mFrontRightEstimator, mBackLeftEstimator;
-//     private AprilTagFieldLayout mFieldLayout  = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-//     private PhotonPipelineResult mFrontRightAprilTagResult, mBackLeftAprilTagResult;
+    private PhotonCamera mFrontRightCam, mBackLeftCam;
+    private PhotonPoseEstimator mFrontRightEstimator, mBackLeftEstimator;
+    private AprilTagFieldLayout mFieldLayout  = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+    private PhotonPipelineResult mFrontRightAprilTagResult, mBackLeftAprilTagResult;
+    private Optional<EstimatedRobotPose> mFrontRight, mBackLeft;
 
-//     public ApriltagVision(){
-//         PhotonCamera.setVersionCheckEnabled(false);
+    public ApriltagVision(){
+        PhotonCamera.setVersionCheckEnabled(false);
 
-//         mFrontRightCam = new PhotonCamera(Constants.Vision.aprilTagFrontRight.camera);
-//         mBackLeftCam = new PhotonCamera(Constants.Vision.aprilTagBackLeft.camera);
+        mFrontRightCam = new PhotonCamera(Constants.Vision.aprilTagFrontRight.camera);
+        mBackLeftCam = new PhotonCamera(Constants.Vision.aprilTagBackLeft.camera);
 
-//         mFrontRightEstimator = new PhotonPoseEstimator(mFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, mFrontRightCam, Constants.Vision.aprilTagFrontRight.robotToCamera);
-//         mBackLeftEstimator = new PhotonPoseEstimator(mFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, mBackLeftCam, Constants.Vision.aprilTagBackLeft.robotToCamera);
+        mFrontRightEstimator = new PhotonPoseEstimator(mFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, mFrontRightCam, Constants.Vision.aprilTagFrontRight.robotToCamera);
+        mBackLeftEstimator = new PhotonPoseEstimator(mFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, mBackLeftCam, Constants.Vision.aprilTagBackLeft.robotToCamera);
 
-//         mFrontRightEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
-//         mBackLeftEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+        mFrontRightEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+        mBackLeftEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
 
-//         mFrontRightEstimator.setTagModel(TargetModel.kAprilTag36h11);
-//         mBackLeftEstimator.setTagModel(TargetModel.kAprilTag36h11);
+        mFrontRightEstimator.setTagModel(TargetModel.kAprilTag36h11);
+        mBackLeftEstimator.setTagModel(TargetModel.kAprilTag36h11);
 
-//         mFrontRightEstimator.setRobotToCameraTransform(Constants.Vision.aprilTagFrontRight.robotToCamera);
-//         mBackLeftEstimator.setRobotToCameraTransform(Constants.Vision.aprilTagBackLeft.robotToCamera);
+        mFrontRightEstimator.setRobotToCameraTransform(Constants.Vision.aprilTagFrontRight.robotToCamera);
+        mBackLeftEstimator.setRobotToCameraTransform(Constants.Vision.aprilTagBackLeft.robotToCamera);
 
         
-//     }
+    }
 
-//     @Override
-//     public void periodic() {
-//         if (this.mFrontRightCam != null){
-//             mFrontRightAprilTagResult = mFrontRightCam.getLatestResult();
-//             mFrontRightEstimator.update(mFrontRightAprilTagResult);
+    @Override
+    public void periodic() {
+        if (this.mFrontRightCam != null){
+            mFrontRightAprilTagResult = mFrontRightCam.getLatestResult();
 
-//             if (mFrontRightAprilTagResult.getMultiTagResult().estimatedPose.isPresent){
+            mFrontRight = getBackLeftEstimatedGlobalPose(Swerve.poseEstimator.getEstimatedPosition(), mFrontRightAprilTagResult);
 
-//                 if (mFrontRightEstimator.update().isPresent()){
-//                     m.poseEstimator.addVisionMeasurement(new Pose2d(, Timer.getFPGATimestamp() - mFrontRightAprilTagResult.getLatencyMillis());
-//                 }  
-                
+            if (mFrontRight.isPresent()){
+                Swerve.poseEstimator.addVisionMeasurement(mFrontRight.get().estimatedPose.toPose2d(), mFrontRightAprilTagResult.getTimestampSeconds());
+            }
 
-//             }
+        }
 
-//         }
+        if (this.mBackLeftCam != null){
+            mBackLeftAprilTagResult = mBackLeftCam.getLatestResult();
 
-//         if (this.mBackLeftCam != null){
-//             mBackLeftAprilTagResult = mBackLeftCam.getLatestResult();
-//             mBackLeftEstimator.update(mBackLeftAprilTagResult);
+            mBackLeft = getFrontRightEstimatedGlobalPose(Swerve.poseEstimator.getEstimatedPosition(), mBackLeftAprilTagResult);
 
-//             if (mBackLeftAprilTagResult.getMultiTagResult().estimatedPose.isPresent){
+            if (mBackLeft.isPresent()){
+                Swerve.poseEstimator.addVisionMeasurement(mBackLeft.get().estimatedPose.toPose2d(), mBackLeftAprilTagResult.getTimestampSeconds());
+            }
 
-//                 if (mBackLeftEstimator.update().isPresent()){
-//                     Swerve.poseEstimator.addVisionMeasurement(mBackLeftEstimator.update().get().estimatedPose.toPose2d(), Timer.getFPGATimestamp() - mBackLeftAprilTagResult.getLatencyMillis());
-//                 }
-//             }
+        }
 
-//         }
+    }
 
-//     }
-// }
+    public Optional<EstimatedRobotPose> getFrontRightEstimatedGlobalPose(Pose2d prevEstimatedRobotPose, PhotonPipelineResult mFrontRightAprilTagResult) {
+        mFrontRightEstimator.setReferencePose(prevEstimatedRobotPose);
+        return mFrontRightEstimator.update(mFrontRightAprilTagResult);
+    }
+
+    public Optional<EstimatedRobotPose> getBackLeftEstimatedGlobalPose(Pose2d prevEstimatedRobotPose, PhotonPipelineResult mBackLeftAprilTagResult) {
+        mBackLeftEstimator.setReferencePose(prevEstimatedRobotPose);
+        return mBackLeftEstimator.update(mBackLeftAprilTagResult);
+    }
+
+    
+}

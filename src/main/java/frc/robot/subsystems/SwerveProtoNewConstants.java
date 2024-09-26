d463e107074497f5009e6218454152a8785eb850
Gaurav Salandri <salangau000@gmail.com>
2024-09-25 19:11:21 -0500

    Added new constants (not implemented)

---
+++ src/main/java/frc/robot/subsystems/SwerveProtoNewConstants.java
@@ -0,0 +1,326 @@
+package frc.robot.subsystems;
+
+import frc.robot.SwerveModule;
+import frc.robot.sim.PhysicsSim;
+import frc.lib.util.COTSTalonFXSwerveConstants.SDS.MK3.driveRatios;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+
+import java.util.Optional;
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathfindHolonomic;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.PathPlannerLogging;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+
+public class SwerveProtoNewConstants extends SubsystemBase {
+    public SwerveDriveOdometry swerveOdometry;
+    public SwerveModule[] mSwerveMods;
+    public Pigeon2 gyro;
+    private Field2d field = new Field2d();
+    public static SwerveDrivePoseEstimator poseEstimator;
+    private final StructArrayPublisher<SwerveModuleState> publisher;
+    Field2d poseEstimateField2d = new Field2d();
+    Pose2d tempPose;
+
+
+    public SwerveProtoNewConstants() {
+        gyro = new Pigeon2(Constants.Swerve.pigeonID, "CanivoreBus");
+        gyro.getConfigurator().apply(new Pigeon2Configuration());
+        gyro.setYaw(0);
+
+        mSwerveMods = new SwerveModule[] {
+            new SwerveModule(0, Constants.Swerve.Mod0.constants),
+            new SwerveModule(1, Constants.Swerve.Mod1.constants),
+            new SwerveModule(2, Constants.Swerve.Mod2.constants),
+            new SwerveModule(3, Constants.Swerve.Mod3.constants)
+        };
+
+        swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics, getGyroYaw(), getModulePositions());
+        
+
+        publisher = NetworkTableInstance.getDefault().getStructArrayTopic("/SwerveStates", SwerveModuleState.struct).publish();
+
+        // Configure the AutoBuilder last
+        AutoBuilder.configureHolonomic(
+        this::getPose, // Robot pose supplier
+        this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+        this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+        this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+        new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+            new PIDConstants(5, 0.0, 0.0), // Translation PID constants
+            new PIDConstants(5, 0.0, 0.0), // Rotation PID constants
+            4.5, // Max module speed, in m/s
+            Constants.Swerve.trackWidth, // Drive base radius in meters. Distance from robot center to furthest module.
+            new ReplanningConfig() // Default path replanning config. See the API for the options here
+               ),
+               () -> {
+                    Optional<Alliance> alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                    return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                 return false;
+            },
+               this // Reference to this subsystem to set requirements
+           );
+
+
+        poseEstimator = new SwerveDrivePoseEstimator(Constants.Swerve.swerveKinematics, getGyroYaw(), getModulePositions(), getPose());
+        // Set up custom logging to add the current path to a field 2d widget
+        PathPlannerLogging.setLogActivePathCallback((poses) -> field.getObject("path").setPoses(poses));
+        SmartDashboard.putData("Field", field);
+        SmartDashboard.putNumber("Heading", getHeading().getDegrees());
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+        SwerveModuleState[] swerveModuleStates =
+            Constants.Swerve.swerveKinematics.toSwerveModuleStates(
+                fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                                    translation.getX(), 
+                                    translation.getY(), 
+                                    rotation, 
+                                    getHeading()
+                                )
+                                : new ChassisSpeeds(
+                                    translation.getX(), 
+                                    translation.getY(), 
+                                    rotation)
+                                );
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
+
+        for(SwerveModule mod : mSwerveMods){
+            mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
+        }
+    }    
+
+    /* Used by SwerveControllerCommand in Auto */
+    public void setModuleStates(SwerveModuleState[] desiredStates) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.Swerve.maxSpeed);
+        
+        for(SwerveModule mod : mSwerveMods){
+            mod.setDesiredState(desiredStates[mod.moduleNumber], false);
+        }
+    }
+
+    public SwerveModuleState[] getModuleStates(){
+        SwerveModuleState[] states = new SwerveModuleState[4];
+        for(SwerveModule mod : mSwerveMods){
+            states[mod.moduleNumber] = mod.getState();
+        }
+        return states;
+    }
+
+    public SwerveModulePosition[] getModulePositions(){
+        SwerveModulePosition[] positions = new SwerveModulePosition[4];
+        for(SwerveModule mod : mSwerveMods){
+            positions[mod.moduleNumber] = mod.getPosition();
+        }
+        return positions;
+    }
+
+    public Pose2d getPose() {
+        if (poseEstimator == null){
+            return swerveOdometry.getPoseMeters();
+        }
+        return poseEstimator.getEstimatedPosition();
+    }
+
+    public void resetOdometry(Pose2d pose) {
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), pose);
+        poseEstimator.resetPosition(getGyroYaw(), getModulePositions(), pose);
+    }
+
+    public Rotation2d getHeading(){
+        return getPose().getRotation();
+    }
+
+    public void setHeading(Rotation2d heading){
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), heading));
+    }
+
+    // This worked in sim testing, but might be redundant and drive() should be used instead, further testing needed
+    public void faceHeading(Rotation2d heading){
+        ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(0, 0, Math.PI, heading);
+        SwerveModuleState[] swerveModuleStates = Constants.Swerve.swerveKinematics.toSwerveModuleStates(speeds);
+        setModuleStates(swerveModuleStates);
+    }
+
+    public void zeroHeading(){
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), new Rotation2d()));
+    }
+
+    public void zeroGyro(){
+        // swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), new Rotation2d()));
+        gyro.setYaw(0);
+    }
+
+    public Rotation2d getGyroYaw() {
+        return Rotation2d.fromDegrees(gyro.getYaw().getValue());
+    }
+
+    public void resetModulesToAbsolute(){ 
+        for(SwerveModule mod : mSwerveMods){
+            mod.resetToAbsolute();
+        }
+    }
+    public ChassisSpeeds getRobotRelativeSpeeds(){
+        return Constants.Swerve.swerveKinematics.toChassisSpeeds(getModuleStates());
+    }
+
+    public void driveRobotRelative(ChassisSpeeds speeds){
+        SwerveModuleState[] states = Constants.Swerve.swerveKinematics.toSwerveModuleStates(speeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, Constants.Swerve.maxSpeed);
+        setModuleStates(states);
+    }
+
+    @Override
+    public void periodic(){
+        swerveOdometry.update(getGyroYaw(), getModulePositions());
+        poseEstimator.updateWithTime(Timer.getFPGATimestamp(), getGyroYaw(), getModulePositions());
+        for(SwerveModule mod : mSwerveMods){
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " CANcoder", mod.getCANcoder().getDegrees());
+            //SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Angle", mod.getPosition().angle.getDegrees());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond); 
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Drive Current", mod.getDriveCurrent());
+            SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Angle Current", mod.getAngleCurrent());      
+        }
+        //SmartDashboard.putNumber("gyro X field", gyro.getMagneticFieldX().getValueAsDouble());
+        //SmartDashboard.putNumber("gyro Y field", gyro.getMagneticFieldY().getValueAsDouble());
+        //SmartDashboard.putNumber("gyro Z field", gyro.getMagneticFieldZ().getValueAsDouble());
+        //SmartDashboard.putNumber("Magnetic North Heading (degrees)", Math.atan2(gyro.getMagneticFieldX().getValueAsDouble(), gyro.getMagneticFieldY().getValueAsDouble()) * 180 / Math.PI);        
+        poseEstimateField2d.setRobotPose(poseEstimator.getEstimatedPosition());
+        // SmartDashboard.putData("s_Swerve", this);
+        SmartDashboard.putData("Estimated Pose", poseEstimateField2d);
+        SmartDashboard.putNumber("Estimated pose x", poseEstimator.getEstimatedPosition().getTranslation().getX());
+        SmartDashboard.putNumber("Estimated pose y", poseEstimator.getEstimatedPosition().getTranslation().getY());
+        SmartDashboard.putNumber("Estimated pose yaw", poseEstimator.getEstimatedPosition().getTranslation().getAngle().getDegrees());
+        SmartDashboard.putNumber("heading", poseEstimator.getEstimatedPosition().getRotation().getDegrees());
+        publisher.set(getModuleStates());
+    }
+
+    /**
+     * Use PathPlanner Path finding to go to a point on the field.
+     *
+     * @param pose Target {@link Pose2d} to go to.
+     * @return PathFinding command
+     */
+    public Command driveToPose(Pose2d pose)
+    {
+        SmartDashboard.putBoolean("it started this driveToPose", true);
+        // Create the constraints to use while pathfinding
+        PathConstraints constraints = new PathConstraints(
+            Constants.AutoConstants.kMaxSpeedMetersPerSecond, 
+            Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared,
+            Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecond,
+            Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecondSquared);
+
+        // return PathfindHolonomic(pose, constraints, 9, getPose(), getRobotRelativeSpeeds(), this::getRobotRelativeSpeeds, this::driveRobotRelative, )
+        // Since AutoBuilder is configured, we can use it to build pathfinding commands
+        return AutoBuilder.pathfindToPose(
+            pose,
+            constraints,
+            0.0, // Goal end velocity in meters/sec
+            0.0); // Rotation delay distance in meters. This is how far the robot should travel before attempting to rotate.                     
+    }
+
+
+        /**
+     * Use PathPlanner Path finding to go to a point on the field.
+     *
+     * @param pose Target {@link Pose2d} to go to.
+     * @return PathFinding command
+     */
+    public Command driveToPose(DoubleSupplier tx, DoubleSupplier ty, DoubleSupplier yaw)
+    {
+        tempPose = new Pose2d(tx.getAsDouble(), ty.getAsDouble(), Rotation2d.fromDegrees(yaw.getAsDouble()));
+
+        SmartDashboard.putString("Target Pose x", tempPose.toString());
+        // Create the constraints to use while pathfinding
+        PathConstraints constraints = new PathConstraints(
+            Constants.AutoConstants.kMaxSpeedMetersPerSecond, 
+            Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared,
+            Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecond,
+            Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecondSquared);
+
+        // Since AutoBuilder is configured, we can use it to build pathfinding commands
+        return AutoBuilder.pathfindToPose(
+            tempPose,
+            constraints,
+            0.0, // Goal end velocity in meters/sec
+            0.0); // Rotation delay distance in meters. This is how far the robot should travel before attempting to rotate.                     
+    }
+
+    public Command rotateToPose(double yaw)
+    {
+        tempPose = new Pose2d(this.getPose().getX(), this.getPose().getY(), Rotation2d.fromDegrees(yaw));
+
+        SmartDashboard.putString("Target Pose x", yaw + "");
+        // Create the constraints to use while pathfinding
+        PathConstraints constraints = new PathConstraints(
+            Constants.AutoConstants.kMaxSpeedMetersPerSecond, 
+            Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared,
+            Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecond,
+            Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecondSquared);
+
+        // Since AutoBuilder is configured, we can use it to build pathfinding commands
+        return AutoBuilder.pathfindToPose(
+            tempPose,
+            constraints,
+            0.0, // Goal end velocity in meters/sec
+            0.0); // Rotation delay distance in meters. This is how far the robot should travel before attempting to rotate.                     
+    }
+
+    // public Command rotateToPose(double yaw)
+    // {
+    //     Pose2d pose = new Pose2d(this.getPose().getTranslation(), Rotation2d.fromDegrees(yaw));
+
+    //     SmartDashboard.putString("Target Pose x", pose.toString());
+    //     // Create the constraints to use while pathfinding
+    //     PathConstraints constraints = new PathConstraints(
+    //         Constants.AutoConstants.kMaxSpeedMetersPerSecond, 
+    //         Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared,
+    //         Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecond,
+    //         Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecondSquared);
+
+    //     // Since AutoBuilder is configured, we can use it to build pathfinding commands
+    //     return AutoBuilder.pathfindToPose(
+    //         pose,
+    //         constraints,
+    //         0.0, // Goal end velocity in meters/sec
+    //         0.0); // Rotation delay distance in meters. This is how far the robot should travel before attempting to rotate.                     
+    // }
+  
+    // USE FOR TESTING ALSO
+    @Override
+    public void simulationPeriodic() {
+        PhysicsSim.getInstance().run();
+    }
+}
